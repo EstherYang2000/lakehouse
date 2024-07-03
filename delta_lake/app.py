44439ab419ba74963ee6ca9d6f2b4aa2b4a69979
esther <ester6126@gmail.com>
2024-07-03 23:12:08 +0800

    [version] simple demo of delta lake

---
+++ delta_lake/app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from deltalake.writer import write_deltalake
+from deltalake import DeltaTable
+
+# Create a Pandas DataFrame
+df = pd.DataFrame({"data": range(5)})
+
+# Write to the Delta Lake table
+write_deltalake("/tmp/deltars_table", df)
+
+# Append new data
+df = pd.DataFrame({"data": range(6, 11)})
+write_deltalake("/tmp/deltars_table", df, mode="append")
+
+# Read the Delta Lake table
+dt = DeltaTable("/tmp/deltars_table")
+
+# Show the Delta Lake table
+print(dt.to_pandas())
