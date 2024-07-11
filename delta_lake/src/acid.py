bf9a37432e30e870313a190d9a9a3d3d20b8a045
YUJIE YANG <80161804+EstherYang2000@users.noreply.github.com>
2024-07-11 16:13:50 +0800

    Update acid.py

--- delta_lake/src/acid.py
+++ delta_lake/src/acid.py
@@ -2,8 +2,9 @@ import pandas as pd
 from deltalake.writer import write_deltalake
 from deltalake import DeltaTable
 import json
+import os
 # Define MinIO endpoint and credentials
-minio_endpoint = "http://localhost:9000"
+minio_endpoint = "http://minio:9000"
 minio_access_key = "esther"
 minio_secret_key = "estheresther"
 minio_bucket = "scpm"
@@ -12,32 +13,24 @@ minio_bucket = "scpm"
 delta_log_path = f"s3a://{minio_bucket}/deltalake_logs"
 delta_table_path = f"s3a://{minio_bucket}/delta_table"
 
-# Set Delta Lake configuration to use MinIO
-# delta_config = {
-#     "logStore": "s3",
-#     "logStore.S3.endpoint": minio_endpoint,
-#     "logStore.S3.accessKey": minio_access_key,
-#     "logStore.S3.secretKey": minio_secret_key,
-#     "logStore.S3.path": delta_log_path,
-# }
-# storage_options = {
-#         "region": "us-east-1",
-#         "endpoint_url": minio_endpoint,
-#         "aws_access_key_id": minio_access_key,
-#         "aws_secret_access_key": minio_secret_key,
-#         "logStore.S3.path": delta_log_path,
-# }
-# Set Delta Lake configuration to use MinIO
-delta_config = {
-    "logStore": "s3",
-    "region" : "",
-    "logStore.S3.endpoint": minio_endpoint,
-    "logStore.S3.accessKey": minio_access_key,
-    "logStore.S3.secretKey": minio_secret_key,
-    "logStore.S3.path": delta_log_path,
+delta_config ={
+
+    "AWS_ENDPOINT_URL": minio_endpoint,
+    "AWS_REGION": "local",
+    "AWS_ACCESS_KEY_ID": minio_access_key,
+    "AWS_SECRET_ACCESS_KEY": minio_secret_key,
+    "AWS_S3_ALLOW_UNSAFE_RENAME": "true",
+    "AWS_ALLOW_HTTP": "true",
+    "AWS_EC2_METADATA_DISABLED": "true",
+
 }
-# Serialize the dictionary into a JSON string
-# storage_options_str = json.dumps(storage_options)
+# os.environ['S3_ACCESS_KEY'] = minio_access_key
+# os.environ['S3_SECRET_KEY'] = minio_secret_key
+# os.environ['S3_ENDPOINT'] = minio_endpoint
+# os.environ['S3_REGION'] = "local"
+# os.environ['S3_PATH_STYLE_ACCESS'] = "true"
+
+
 
 # Create a Pandas DataFrame
 df = pd.DataFrame({"id": [1, 2, 3], "name": ["Alice", "Bob", "Charlie"]})
@@ -46,9 +39,9 @@ df = pd.DataFrame({"id": [1, 2, 3], "name": ["Alice", "Bob", "Charlie"]})
 write_deltalake(delta_table_path, df,storage_options=delta_config)
 
 # Append new data to existing Delta Lake table in MinIO
-df_append = pd.DataFrame({"id": [4, 5], "name": ["David", "Eve"]})
-write_deltalake(delta_table_path, df_append, mode="append",storage_options=delta_config)
+# df_append = pd.DataFrame({"id": [4, 5], "name": ["David", "Eve"]})
+# write_deltalake(delta_table_path, df_append, mode="overwrite",storage_options=delta_config)
 
 # Read and display the Delta Lake table from MinIO
-dt = DeltaTable(delta_table_path, storage_options=delta_config)
+dt = DeltaTable(delta_table_path,storage_options=delta_config)
 print(dt.to_pandas())
