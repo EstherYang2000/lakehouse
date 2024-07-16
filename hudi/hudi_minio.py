b058cf98b46c852cf08b6a7df7d2a1968d86cb00
YUJIE YANG <80161804+EstherYang2000@users.noreply.github.com>
2024-07-16 17:33:33 +0800

    Update hudi_minio.py

--- hudi/hudi_minio.py
+++ hudi/hudi_minio.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import lit
+from pyspark.sql.functions import lit, col  # 导入 col 函数
+from pyspark.sql.types import StructType, StructField, LongType, StringType, FloatType
+
 # pyspark 3.4.0
 # Define MinIO endpoint and credentials
 minio_endpoint = "http://localhost:9000"
@@ -24,6 +26,18 @@ spark = SparkSession.builder \
 
 print(spark.version)
 
+
+# 定義數據架構
+schema = StructType([
+    StructField("ts", LongType(), True),
+    StructField("uuid", StringType(), True),
+    StructField("rider", StringType(), True),
+    StructField("driver", StringType(), True),
+    StructField("fare", FloatType(), True),
+    StructField("city", StringType(), True)
+])
+
+
 # DataFrame data
 columns = ["ts","uuid","rider","driver","fare","city"]
 data =[
@@ -33,28 +47,33 @@ data =[
     (1695516137016,"e3cf430c-889d-4015-bc98-59bdce1e530c","rider-F","driver-P",34.15,"sao_paulo"),
     (1695115999911,"c8abbe79-8d89-47ea-b4ce-4d224bae5bfa","rider-J","driver-T",17.85,"chennai")
 ]
-inserts = spark.createDataFrame(data).toDF(*columns)
+# 1.創建 DataFrame
+inserts = spark.createDataFrame(data, schema)
 
+table = "test_cow_table_minio"
 # Configure Hudi options with Hive sync disabled
 hudi_options = {
-    'hoodie.table.name': "test_table",
+    'hoodie.table.name': table,
+    'hoodie.datasource.write.table.type':"COPY_ON_WRITE",
     'hoodie.datasource.write.recordkey.field': 'ts',
     'hoodie.datasource.write.partitionpath.field': 'city',
-    'hoodie.insert.shuffle.parallelism': '2',
+    'hoodie.insert.shuffle.parallelism': '10',
+    'hoodie.upsert.shuffle.parallelism': '10',
+    'hoodie.delete.shuffle.parallelism': '10',
     'hoodie.datasource.hive_sync.enable': 'false',  # Disable Hive sync
-    'hoodie.datasource.write.hive_style_partitioning': 'true'
+    'hoodie.datasource.write.hive_style_partitioning': 'false'
 }
 
-basePath = f"s3a://{minio_bucket}/test_table"
+basePath = f"s3a://{minio_bucket}/{table}"
 
-## insert data to hudi table
+# # 1. insert data to hudi table
 # inserts.write.format("hudi"). \
 #     options(**hudi_options). \
 #     mode("overwrite"). \
 #     save(basePath)
 
 
-# Upsert Data
+# 2. Upsert Data
 # upsert_data = [
 #     (1695159649087, "334e26e9-8355-45cc-97c6-c31daf0df330", "rider-A", "driver-K", 20.10, "san_francisco"),  # Update fare
 #     (1695091554788, "e96c4396-3fad-413a-a942-4cb36106d721", "rider-C", "driver-M", 28.70, "san_francisco"),  # Update fare
@@ -66,9 +85,18 @@ basePath = f"s3a://{minio_bucket}/test_table"
 #     mode("append"). \
 #     save(basePath)
 
-# Delete Data
+# 3.Update data
+# Lets read data from target Hudi table, modify fare column for rider-D and update it.
+# updatesDf = spark.read.format("hudi").load(basePath).filter("rider == 'rider-D'").withColumn("fare",col("fare")*10)
+
+# updatesDf.write.format("hudi"). \
+#   options(**hudi_options). \
+#   mode("append"). \
+#   save(basePath)
+
+# 4. Delete Data
 delete_data = [
-    (1695159649087, "334e26e9-8355-45cc-97c6-c31daf0df330", "rider-A", "driver-K", 20.10, "san_francisco"),  # Match schema
+    (1695159649087, "334e26e9-8355-45cc-97c6-c31daf0df330", "rider-A", "driver-K", 20.1, "san_francisco"),  # Match schema
     (1695091554788, "e96c4396-3fad-413a-a942-4cb36106d721", "rider-C", "driver-M", 27.70, "san_francisco")   # Match schema
 ]
 deletes = spark.createDataFrame(delete_data).toDF(*columns)
