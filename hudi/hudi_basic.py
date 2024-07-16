b1dae2e54c1ed8dc2ef98862d8a543859444afb6
YUJIE YANG <80161804+EstherYang2000@users.noreply.github.com>
2024-07-16 17:33:00 +0800

    Update hudi_basic.py

--- hudi/hudi_basic.py
+++ hudi/hudi_basic.py
@@ -1,151 +1,134 @@
-from typing import *
-
-from pyspark import Row
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import lit, col  # 导入 col 函数
+from pyspark.sql.types import StructType, StructField, LongType, StringType, FloatType
 
+# pyspark 3.4.0
+# Define MinIO endpoint and credentials
+minio_endpoint = "http://localhost:9000"
+minio_access_key = "esther"
+minio_secret_key = "estheresther"
+minio_bucket = "scpm"
+
+# Initialize SparkSession with Hudi and Hadoop AWS packages
 spark = SparkSession.builder \
-    .appName("Hudi Basics") \
+    .appName("Hudi with MinIO") \
     .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
-    .config("spark.jars.packages", "org.apache.hudi:hudi-spark3.3-bundle_2.12:0.13.1") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.hudi.catalog.HoodieCatalog") \
-    .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension") \
-    .config("spark.sql.legacy.timeParserPolicy", "LEGACY") \
-    .master("local[*]") \
+    .config("spark.sql.hive.convertMetastoreParquet", "false") \
+    .config("spark.hadoop.fs.s3a.access.key", minio_access_key) \
+    .config("spark.hadoop.fs.s3a.secret.key", minio_secret_key) \
+    .config("spark.hadoop.fs.s3a.endpoint", minio_endpoint) \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.jars.packages", "org.apache.hudi:hudi-spark3.4-bundle_2.12:0.14.0,org.apache.hadoop:hadoop-aws:3.3.4") \
+    .config("spark.jars.excludes", "org.apache.hadoop:hadoop-common,org.apache.hadoop:hadoop-mapreduce-client-core") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
     .getOrCreate()
 
-sc = spark.sparkContext
-
-table_name = "hudi_trips_cow"
-base_path = "./data/hudi_trips_cow"
-quickstart_utils = sc._jvm.org.apache.hudi.QuickstartUtils
-dataGen = quickstart_utils.DataGenerator()
-
-inserts = sc._jvm.org.apache.hudi.QuickstartUtils.convertToStringList(dataGen.generateInserts(10))
+print(spark.version)
 
 
-def create_df():
-    df = spark.read.json(spark.sparkContext.parallelize(inserts, 2))
-    return df
+# 定義數據架構
+schema = StructType([
+    StructField("ts", LongType(), True),
+    StructField("uuid", StringType(), True),
+    StructField("rider", StringType(), True),
+    StructField("driver", StringType(), True),
+    StructField("fare", FloatType(), True),
+    StructField("city", StringType(), True)
+])
 
 
-def write_data():
-    df = create_df()
+# DataFrame data
+columns = ["ts","uuid","rider","driver","fare","city"]
+data =[
+    (1695159649087,"334e26e9-8355-45cc-97c6-c31daf0df330","rider-A","driver-K",19.10,"san_francisco"),
+    (1695091554788,"e96c4396-3fad-413a-a942-4cb36106d721","rider-C","driver-M",27.70 ,"san_francisco"),
+    (1695046462179,"9909a8b1-2d15-4d3d-8ec9-efc48c536a00","rider-D","driver-L",33.90 ,"san_francisco"),
+    (1695516137016,"e3cf430c-889d-4015-bc98-59bdce1e530c","rider-F","driver-P",34.15,"sao_paulo"),
+    (1695115999911,"c8abbe79-8d89-47ea-b4ce-4d224bae5bfa","rider-J","driver-T",17.85,"chennai")
+]
+# 1.創建 DataFrame
+inserts = spark.createDataFrame(data, schema)
 
-    hudi_options = {
-        'hoodie.table.name': table_name,
-        'hoodie.datasource.write.recordkey.field': 'uuid',
-        'hoodie.datasource.write.partitionpath.field': 'partitionpath',
-        'hoodie.datasource.write.table.name': table_name,
-        'hoodie.datasource.write.operation': 'upsert',
-        'hoodie.datasource.write.precombine.field': 'ts',
-        'hoodie.upsert.shuffle.parallelism': 2,
-        'hoodie.insert.shuffle.parallelism': 2
-    }
+table = "test_cow_np_table_minio"
+# Configure Hudi options with Hive sync disabled
+hudi_options = {
+    'hoodie.table.name': table,
+    'hoodie.datasource.write.table.type':"COPY_ON_WRITE",
+    'hoodie.datasource.write.recordkey.field': 'ts',
+    'hoodie.insert.shuffle.parallelism': '10',
+    'hoodie.upsert.shuffle.parallelism': '10',
+    'hoodie.delete.shuffle.parallelism': '10',
+    'hoodie.datasource.hive_sync.enable': 'false',  # Disable Hive sync
+    'hoodie.datasource.write.hive_style_partitioning': 'false'
+}
 
-    df.write.format("hudi") \
-        .options(**hudi_options) \
-        .mode("overwrite") \
-        .save(base_path)
+basePath = f"s3a://{minio_bucket}/{table}"
 
-
-def query_data():
-    trips_snapshot_df = spark.read \
-        .format("hudi") \
-        .load(base_path)
-
-    trips_snapshot_df \
-        .where(col("fare") > 20.0) \
-        .select(
-            col("fare"),
-            col("begin_lon"),
-            col("begin_lat"),
-            col("ts")) \
-        .show(truncate=False)
-
-    trips_snapshot_df \
-        .select(
-            col("_hoodie_commit_time"),
-            col("_hoodie_record_key"),
-            col("_hoodie_partition_path"),
-            col("rider"),
-            col("driver"),
-            col("fare")) \
-        .show(truncate=False)
+# # 1. insert data to hudi table
+inserts.write.format("hudi"). \
+    options(**hudi_options). \
+    mode("overwrite"). \
+    save(basePath)
 
 
-def time_travel_query():
-    spark.read \
-        .format("hudi") \
-        .option("as.of.instant", "20230919095758730") \
-        .load(base_path) \
-        .show()
+# 2. Upsert Data
+# upsert_data = [
+#     (1695159649087, "334e26e9-8355-45cc-97c6-c31daf0df330", "rider-A", "driver-K", 20.10, "san_francisco"),  # Update fare
+#     (1695091554788, "e96c4396-3fad-413a-a942-4cb36106d721", "rider-C", "driver-M", 28.70, "san_francisco"),  # Update fare
+#     (1695200000000, "new-uuid-1", "rider-Z", "driver-Y", 45.50, "new_york")  # New record
+# ]
+# upserts = spark.createDataFrame(upsert_data).toDF(*columns)
+# upserts.write.format("hudi"). \
+#     options(**hudi_options). \
+#     mode("append"). \
+#     save(basePath)
 
-    spark.read \
-        .format("hudi") \
-        .option("as.of.instant", "2023-09-19 09:57:58.730") \
-        .load(base_path) \
-        .show()
+# 3.Update data
+# Lets read data from target Hudi table, modify fare column for rider-D and update it.
+# updatesDf = spark.read.format("hudi").load(basePath).filter("rider == 'rider-D'").withColumn("fare",col("fare")*10)
 
-    # clock starts at 00:00:00.000
-    spark.read \
-        .format("hudi") \
-        .option("as.of.instant", "2023-09-20") \
-        .load(base_path) \
-        .show()
+# updatesDf.write.format("hudi"). \
+#   options(**hudi_options). \
+#   mode("append"). \
+#   save(basePath)
 
+# 4. Delete Data
+# delete_data = [
+#     (1695159649087, "334e26e9-8355-45cc-97c6-c31daf0df330", "rider-A", "driver-K", 20.1, "san_francisco"),  # Match schema
+#     (1695091554788, "e96c4396-3fad-413a-a942-4cb36106d721", "rider-C", "driver-M", 27.70, "san_francisco")   # Match schema
+# ]
+# deletes = spark.createDataFrame(delete_data).toDF(*columns)
 
-def update_data():
-    updates = quickstart_utils.convertToStringList(dataGen.generateUpdates(10))
-    df = spark.read.json(spark.sparkContext.parallelize(updates, 2))
-    df.write \
-        .format("hudi") \
-        .mode("append") \
-        .save(base_path)
+# deletes = deletes.withColumn("_hoodie_is_deleted", lit(True))
 
-    query_data()
+# deletes.write.format("hudi"). \
+#     options(**hudi_options). \
+#     mode("append"). \
+#     option("hoodie.datasource.write.operation", "delete") \
+#     .save(basePath)
 
+# Read Hudi table
+hudi_df = spark.read.format("hudi").load(basePath)
+hudi_df.show()
 
-def incremental_query():
-    ordered_rows: list[Row] = spark.read \
-        .format("hudi") \
-        .load(base_path) \
-        .select(col("_hoodie_commit_time").alias("commit_time")) \
-        .orderBy(col("commit_time")) \
-        .collect()
+# To enable Hive sync, ensure Hive Metastore is running and configured correctly.
+# Uncomment and configure the following options if Hive sync is needed.
+"""
+hudi_options = {
+    'hoodie.table.name': "test_table",
+    'hoodie.datasource.write.recordkey.field': 'ts',
+    'hoodie.datasource.write.partitionpath.field': 'city',
+    'hoodie.insert.shuffle.parallelism': '2',
+    'hoodie.datasource.hive_sync.enable': 'true',
+    'hoodie.datasource.hive_sync.table': 'test_table',
+    'hoodie.datasource.hive_sync.partition_fields': 'city',
+    'hoodie.datasource.hive_sync.jdbcurl': 'jdbc:hive2://localhost:10000',
+    'hoodie.datasource.hive_sync.username': 'hive',
+    'hoodie.datasource.hive_sync.password': 'hive',
+    'hoodie.datasource.hive_sync.database': 'default',
+    'hoodie.datasource.hive_sync.metastore.uris': 'thrift://localhost:9083'
+}
+"""
 
-    commits: list[Any] = list(map(lambda row: row[0], ordered_rows))
-    begin_time = commits[0]
-
-    # print(commits)
-    # print(begin_time)
-
-    incremental_read_options = {
-        'hoodie.datasource.query.type': 'incremental',
-        'hoodie.datasource.read.begin.instanttime': begin_time,
-    }
-
-    trips_incremental_df = spark.read \
-        .format("hudi") \
-        .options(**incremental_read_options) \
-        .load(base_path)
-
-    trips_incremental_df.show()
-
-    trips_incremental_df \
-        .where(col("fare") > 20.0) \
-        .select(col("_hoodie_commit_time"),
-                col("fare"),
-                col("begin_lon"),
-                col("begin_lat"),
-                col("ts")) \
-        .show()
-
-
-if __name__ == '__main__':
-    create_df()
-    # write_data()
-    # query_data()
-    # time_travel_query()
-    # update_data()
-    # incremental_query()
+spark.stop()
