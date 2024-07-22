4f64a488397b439d6f2ba62d7bd09630ba41b2ad
esther <ester6126@gmail.com>
2024-07-23 00:04:29 +0800

    [POC] finish the create the delta table

---
+++ delta_lake/src/delta.py
@@ -0,0 +1,38 @@
+import pyarrow as pa
+import os
+from deltalake import DeltaTable
+
+
+def convert_to_arrow_schema(schema_yaml: dict) -> pa.Schema:
+    # Parse the schema
+    fields = []
+    for field in schema_yaml['schema']:
+        field_name = field['name']
+        field_type = field['type']
+        
+        if field_type == 'int32':
+            pa_type = pa.int32()
+        elif field_type == 'string':
+            pa_type = pa.string()
+        elif field_type == 'float64':
+            pa_type = pa.float64()
+        elif field_type == 'timestamp':
+            pa_type = pa.date64()
+        else:
+            raise ValueError(f"Unsupported type: {field_type}")
+        
+        fields.append((field_name, pa_type))
+    schema = pa.schema(fields)
+    return schema
+
+def is_delta_table(path):
+    if os.path.exists(path):
+        try:
+            delta_table = DeltaTable(path)
+            return True
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
+    return False
+
+        
--- delta_lake/src/sdk.py
+++ delta_lake/src/sdk.py
@@ -4,6 +4,8 @@ from deltalake import DeltaTable
 import json
 import os
 from delta_lake.utils.read_data import read_file,read_yaml
+from delta_lake.src.delta import convert_to_arrow_schema,is_delta_table
+from datetime import datetime, timedelta
 
 if __name__ == "__main__":
     # Construct Delta Lake paths
@@ -12,23 +14,24 @@ if __name__ == "__main__":
     source_path = yaml_data["delta_table"]["delta_table_source_path"]
     table_name = source_path.split('/')[-1].split('.')[-2]
     delta_table = os.path.join(yaml_data["delta_table"]["delta_table_path"],)
-    delta_table_path = f"lakehouse/delta_lake/data/scmp-stage-layer/cpo/ba_dmnd_data/{table_name}"
-
+    delta_table_path = f"data/scmp-stage-layer/cpo/ba_dmnd_data/{table_name}"
+    source_df = read_file(source_path)
+    # Convert all datetime64[ns] columns to datetime64[s]
+    print(source_df.dtypes)
+    for col in source_df.select_dtypes(include=['datetime64[ns]']).columns:
+        source_df[col] = source_df[col].astype('datetime64[ns]')
+    schema_yaml = read_yaml('config/schema.yaml')
+    schema_pa = convert_to_arrow_schema(schema_yaml)
+    print(schema_pa)
+    delta_config ={}
     if not os.path.exists(delta_table_path):
-        print(f"Delta Table Directory '{directory}' does not exist.")
+        print(f"Delta Table Directory '{delta_table_path}' does not exist.")
         os.makedirs(delta_table_path, exist_ok=True)
-        
+    # Check if the Delta table exists
+    if is_delta_table(delta_table_path):
+        print("Delta table exists at the specified path.")
     else:
-
-
-        # Define schema for the Delta Lake table 
-        # schema = pa.schema([
-        #     ("id", pa.int32()),
-        #     ("name", pa.string())
-        # ])
-        # Create a Pandas DataFrame
-        # df = pd.DataFrame({"id": [1, 2, 3], "name": ["Alice", "Bob", "Charlie"]})
-        # df = df.astype({"id": "int32", "name": "string"})  # Ensure Pandas dtype matches Spark schema
-
+        print("Delta table does not exist at the specified path.")
         # Write DataFrame to Delta Lake table in MinIO
-        # write_deltalake(delta_table_path, df,schema=schema,storage_options=delta_config)
+        write_deltalake(delta_table_path, source_df,schema=schema_pa,storage_options=delta_config)
+        
